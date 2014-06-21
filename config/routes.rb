aadbf09256f9c749888797469bf46c29e346e997
Deirdre Lyons <lyonsdee@gmail.com>
2014-06-21 15:51:43 +0100

    deirdre

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsgirlsGalway2014::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,7 +7,7 @@ RailsgirlsGalway2014::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+   get '/' => 'application#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
